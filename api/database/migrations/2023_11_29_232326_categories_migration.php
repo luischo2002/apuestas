ef66a9749a48dc6c558dd4062fda71f52bb172c0
luischo2002 <115508671+luischo2002@users.noreply.github.com>
2023-11-29 17:39:41 -0600

    Base de datos y migraciones

--- api/database/migrations/2023_11_29_232326_categories_migration.php
+++ api/database/migrations/2023_11_29_232326_categories_migration.php
@@ -11,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+        Schema::create('categorys', function (Blueprint $table) {
+            $table->increments('id_category');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -27,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categorys');
     }
 };
