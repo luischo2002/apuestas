e121a67b1df9622ac00a9ce810e95a7927c7e493
luischo2002 <115508671+luischo2002@users.noreply.github.com>
2023-12-04 15:10:56 -0600

    Pagina de inicio

--- apuestas-front/src/components/HelloWorld.vue
+++ apuestas-front/src/components/HelloWorld.vue
@@ -1,34 +1,169 @@
 <template>
-  <HeaderComponent/>
-  <div class="hello">
-    
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <HeaderComponent />
+  <div class="container">
+    <h2>Apuesta y gana</h2>
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th>Partidos
+          </th>
+          <th>Opciones</th>
+          <th>Apostar</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Equipo 1 <br> Equipo 2</td>
+          <td>
+            <button type="button" class="btn btn-danger">Equipo 1</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Empate</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Equipo 2</button>
+          </td>
+
+          <td>
+            <input type="text" class="form-control" placeholder="Apostar">
+          </td>
+
+        </tr>
+        <tr>
+          <td>Equipo 1 <br> Equipo 2</td>
+          <td>
+            <button type="button" class="btn btn-danger">Equipo 1</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Empate</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Equipo 2</button>
+          </td>
+
+          <td>
+            <input type="text" class="form-control" placeholder="Apostar">
+          </td>
+
+        </tr>
+        <tr>
+          <td>Equipo 1 <br> Equipo 2</td>
+          <td>
+            <button type="button" class="btn btn-danger">Equipo 1</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Empate</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Equipo 2</button>
+          </td>
+
+          <td>
+            <input type="text" class="form-control" placeholder="Apostar">
+          </td>
+
+        </tr>
+        <tr>
+          <td>Equipo 1 <br> Equipo 2</td>
+          <td>
+            <button type="button" class="btn btn-danger">Equipo 1</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Empate</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Equipo 2</button>
+          </td>
+
+          <td>
+            <input type="text" class="form-control" placeholder="Apostar">
+          </td>
+
+        </tr>
+        <tr>
+          <td>Equipo 1 <br> Equipo 2</td>
+          <td>
+            <button type="button" class="btn btn-danger">Equipo 1</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Empate</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Equipo 2</button>
+          </td>
+
+          <td>
+            <input type="text" class="form-control" placeholder="Apostar">
+          </td>
+
+        </tr>
+        <tr>
+          <td>Equipo 1 <br> Equipo 2</td>
+          <td>
+            <button type="button" class="btn btn-danger">Equipo 1</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Empate</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Equipo 2</button>
+          </td>
+
+          <td>
+            <input type="text" class="form-control" placeholder="Apostar">
+          </td>
+
+        </tr>
+        <tr>
+          <td>Equipo 1 <br> Equipo 2</td>
+          <td>
+            <button type="button" class="btn btn-danger">Equipo 1</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Empate</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Equipo 2</button>
+          </td>
+
+          <td>
+            <input type="text" class="form-control" placeholder="Apostar">
+          </td>
+
+        </tr>
+        <tr>
+          <td>Equipo 1 <br> Equipo 2</td>
+          <td>
+            <button type="button" class="btn btn-danger">Equipo 1</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Empate</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Equipo 2</button>
+          </td>
+
+          <td>
+            <input type="text" class="form-control" placeholder="Apostar">
+          </td>
+
+        </tr>
+        <tr>
+          <td>Equipo 1 <br> Equipo 2</td>
+          <td>
+            <button type="button" class="btn btn-danger">Equipo 1</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Empate</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Equipo 2</button>
+          </td>
+
+          <td>
+            <input type="text" class="form-control" placeholder="Apostar">
+          </td>
+
+        </tr>
+        <tr>
+          <td>Equipo 1 <br> Equipo 2</td>
+          <td>
+            <button type="button" class="btn btn-danger">Equipo 1</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Empate</button>
+            <span style="margin-right: 10px;"></span>
+            <button type="button" class="btn btn-danger">Equipo 2</button>
+          </td>
+
+          <td>
+            <input type="text" class="form-control" placeholder="Apostar">
+          </td>
+
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -36,11 +171,11 @@
 import HeaderComponent from './HeaderComponent.vue';
 
 export default {
-    name: 'HelloWorld',
-    props: {
-        msg: String
-    },
-    components: { HeaderComponent }
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: { HeaderComponent }
 }
 </script>
 
@@ -49,14 +184,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
