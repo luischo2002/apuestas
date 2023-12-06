c599bd2f31e897cf1bf9d747a6a036918878e0b6
luischo2002 <115508671+luischo2002@users.noreply.github.com>
2023-12-06 16:50:00 -0600

    Inicio conectado a la BD: 2

--- apuestas-front/src/components/HelloWorld.vue
+++ apuestas-front/src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <HeaderComponent />
   <img alt="Vue logo" src="../assets/img/logo.png">
   <div class="container">
-    <RetiroModalComponent/>
+    <RetiroModalComponent />
     <h2>¡Apuesta y gana!</h2>
     <table class="table table-bordered">
       <thead>
@@ -14,149 +14,15 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Cruz Azul <br> Juventus</td>
+        <tr v-for="p in data" v-bind:key="p.id">
+          <td>{{ p.equipo1 }} <br>{{ p.equipo2 }} </td>
+
           <td>
-            <button type="button" class="btn btn-danger">Cruz Azul</button>
+            <button type="button" class="btn btn-danger">{{ p.equipo1 }}</button>
             <span style="margin-right: 10px;"></span>
             <button type="button" class="btn btn-danger">Empate</button>
             <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">Juventus</button>
-          </td>
-
-          <td>
-            <input type="text" class="form-control" placeholder="Apostar">
-          </td>
-
-        </tr>
-        <tr>
-          <td>Real Madrid <br> Atlas</td>
-          <td>
-            <button type="button" class="btn btn-danger">Real Madrid</button>
-            <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">Empate</button>
-            <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">Atlas</button>
-          </td>
-
-          <td>
-            <input type="text" class="form-control" placeholder="Apostar">
-          </td>
-
-        </tr>
-        <tr>
-          <td>PSG <br> FC Juárez</td>
-          <td>
-            <button type="button" class="btn btn-danger">PSG</button>
-            <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">Empate</button>
-            <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">FC Juárez</button>
-          </td>
-
-          <td>
-            <input type="text" class="form-control" placeholder="Apostar">
-          </td>
-
-        </tr>
-        <tr>
-          <td>Liverpool <br> América</td>
-          <td>
-            <button type="button" class="btn btn-danger">Liverpool</button>
-            <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">Empate</button>
-            <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">América</button>
-          </td>
-
-          <td>
-            <input type="text" class="form-control" placeholder="Apostar">
-          </td>
-
-        </tr>
-        <tr>
-          <td>Inter Miami <br> Pumas UNAM</td>
-          <td>
-            <button type="button" class="btn btn-danger">Inter Miami</button>
-            <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">Empate</button>
-            <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">Pumas UNAM</button>
-          </td>
-
-          <td>
-            <input type="text" class="form-control" placeholder="Apostar">
-          </td>
-
-        </tr>
-        <tr>
-          <td>Manchester United <br> Santos Laguna</td>
-          <td>
-            <button type="button" class="btn btn-danger">Manchester United</button>
-            <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">Empate</button>
-            <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">Santos Laguna</button>
-          </td>
-
-          <td>
-            <input type="text" class="form-control" placeholder="Apostar">
-          </td>
-
-        </tr>
-        <tr>
-          <td>Bayern Munich <br> Tigres UANL</td>
-          <td>
-            <button type="button" class="btn btn-danger">Bayern Munich</button>
-            <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">Empate</button>
-            <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">Tigres UANL</button>
-          </td>
-
-          <td>
-            <input type="text" class="form-control" placeholder="Apostar">
-          </td>
-
-        </tr>
-        <tr>
-          <td>FC Barcelona <br> Puebla</td>
-          <td>
-            <button type="button" class="btn btn-danger">FC Barcelona</button>
-            <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">Empate</button>
-            <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">Puebla</button>
-          </td>
-
-          <td>
-            <input type="text" class="form-control" placeholder="Apostar">
-          </td>
-
-        </tr>
-        <tr>
-          <td>Boca Juniors <br> Necaxa</td>
-          <td>
-            <button type="button" class="btn btn-danger">Boca Juniors</button>
-            <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">Empate</button>
-            <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">Necaxa</button>
-          </td>
-
-          <td>
-            <input type="text" class="form-control" placeholder="Apostar">
-          </td>
-
-        </tr>
-        <tr>
-          <td>Arsenal <br> Chivas</td>
-          <td>
-            <button type="button" class="btn btn-danger">Arsenal</button>
-            <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">Empate</button>
-            <span style="margin-right: 10px;"></span>
-            <button type="button" class="btn btn-danger">Chivas</button>
+            <button type="button" class="btn btn-danger">{{ p.equipo2 }}</button>
           </td>
 
           <td>
@@ -172,34 +38,35 @@
 <script>
 import HeaderComponent from './HeaderComponent.vue';
 import RetiroModalComponent from './RetiroModalComponent.vue';
+import axios from 'axios'
 
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+
+
+  components: {
+    HeaderComponent,
+    RetiroModalComponent
   },
-  components: { HeaderComponent , RetiroModalComponent }
+  data() {
+    return {
+      data: [],
+      url: "http://localhost:8000/api/",
+
+    }
+  },
+  methods: {
+    getData() {
+      axios.get('apuestas').then(res => {
+        if (res.data.status == 'success') {
+          this.data = res.data.data
+        }
+      })
+    }
+  },
+  mounted() {
+    this.getData()
+  }
+
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-
-</style>
--- apuestas-front/src/components/RetiroModalComponent.vue
+++ apuestas-front/src/components/RetiroModalComponent.vue
@@ -1,87 +1,86 @@
 <template>
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
+  <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Retiro
+  </button>
 
-                    <!-- Modal -->
-                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Retiro de fondos</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Retiro de fondos</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form>
+
+            <label class="form-label">Método de retiro</label>
+            <div class="mb-3">
+              <select name="" class="form-select" v-model="txtMetodo">
+                <option selected value="Clabe">Clabe</option>
+              </select>
+            </div>
+            <label for="" class="form-label">Información de retiro</label>
+            <div class="row g-2">
+              <div class="col md">
+                <div class="form-floating mb-3">
+                  <input type="text" class="form-control" id="nom_banco" v-model="txtNomBan">
+                  <label for="nom_banco">Nombre del banco</label>
+                </div>
+              </div>
+              <div class="col md">
+                <div class="form-floating mb-3">
+                  <input type="number" class="form-control" id="cuenta_clabe" v-model="txtClabe">
+                  <label for="cuenta_clabe">Cuenta Clabe</label>
+                </div>
+              </div>
+            </div>
+            <div class="form-floating mb-3">
+              <input type="text" class="form-control" id="titular_cuenta" v-model="txtTitular">
+              <label for="titular_cuenta">Titular de la cuenta</label>
+            </div>
+
+            <label for="" class="form-label">Cantidad a retirar</label>
+            <div class="form-floating mb-3">
+              <input type="number" class="form-control" id="cantidad_a_retirar" placeholder="0" min="0" max="100000"
+                v-model="txtCantidad">
+              <label for="cantidad_a_retirar">Ingrese la cantidad</label>
+            </div>
+
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+          <button @click="retirar()" class="btn btn-primary">Retirar</button>
+        </div>
       </div>
-      <div class="modal-body">
-        <form>
-            
-    <label class="form-label" >Método de retiro</label>
-  <div class="mb-3">
-    <select name="" class="form-select" v-model="txtMetodo">
-        <option selected value="Clabe" >Clabe</option>
-    </select>
-  </div>
-  <label for="" class="form-label">Información de retiro</label>
-  <div class="row g-2">
-    <div class="col md">
-        <div class="form-floating mb-3">
-  <input type="text" class="form-control" id="nom_banco" v-model="txtNomBan">
-  <label for="nom_banco">Nombre del banco</label>
-</div>
     </div>
-<div class="col md">
-    <div class="form-floating mb-3">
-  <input type="number" class="form-control" id="cuenta_clabe" v-model="txtClabe">
-  <label for="cuenta_clabe">Cuenta Clabe</label>
-</div>
-</div>
   </div>
-<div class="form-floating mb-3">
-  <input type="text" class="form-control" id="titular_cuenta" v-model="txtTitular">
-  <label for="titular_cuenta">Titular de la cuenta</label>
-</div>
-
-<label for="" class="form-label">Cantidad a retirar</label>
-<div class="form-floating mb-3">
-  <input type="number" class="form-control" id="cantidad_a_retirar" placeholder="0" min="0" max="100000" v-model="txtCantidad">
-  <label for="cantidad_a_retirar">Ingrese la cantidad</label>
-</div>
-
-</form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-  <button @click="retirar()" class="btn btn-primary">Retirar</button>
-      </div>
-    </div>
-  </div>
-</div>
-
 </template>
 
 <script>
 import axios from 'axios';
 export default {
-        name: 'RetiroModalComponent',
-        methods:{
-        retirar(){
-            axios.put('users/1',{
-              balance:this.txtCantidad
-            }).then(res=>{
-                if(res.data.status=='success'){
-                    //codigo pa pagar jeje
-                    console.log("Eres el papu de papus")
-                }else{
-                  console.log(res.data)
-                }
-            });
+  name: 'RetiroModalComponent',
+  methods: {
+    retirar() {
+      axios.put('users/1', {
+        balance: this.txtCantidad
+      }).then(res => {
+        if (res.data.status == 'success') {
+          //codigo pa pagar jeje
+          console.log("Eres el papu de papus")
+        } else {
+          console.log(res.data)
         }
+      });
     }
-    }
-    
+  }
+}
+
 </script>
 <style scoped>
-
-.btn.btn-primary{
-  margin-bottom:40px;
+.btn.btn-primary {
+  margin-bottom: 40px;
 }
 </style>
